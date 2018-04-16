52e84c2e0e5e5bee6330131bcecc48e5c83114ee
Brandon Kupczyk <grandbman1@gmail.com>
2018-04-16 11:54:56 -0500

    Made the max output file size no larger than ~30MB

--- makeHumanReadable.py
+++ makeHumanReadable.py
@@ -2,6 +2,7 @@ import msgpack
 import csv
 import datetime
 import sys
+import os
 
 #"c:\Python27\python.exe" yourprogram.py %1 # FOR DRAG AND DROP BATCH FILE
 
@@ -11,7 +12,17 @@ unpacker = msgpack.Unpacker(fr)
 
 
 fn = fileName + '.csv'
-with open(fn, "w") as f:
-    writer = csv.writer(f)
-    for o in unpacker:
-        writer.writerows(o)
+fileCnt = 1
+f = open(fn, "w+")
+writer = csv.writer(f)
+
+for o in unpacker:
+    if os.path.getsize(fn) >= 30000000:
+        f.close()
+        fn = fileName+str(fileCnt)+'.csv'
+        f = open(fn, "w+")
+        writer = csv.writer(f)
+        fileCnt = fileCnt + 1
+    writer.writerows(o)
+
+f.close()
